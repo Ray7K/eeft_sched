c6e10d21e88499dcb4c3ccd336756049dfd6088c
Ray7K <173786719+Ray7K@users.noreply.github.com>
2025-09-04 08:41:58 +0530

    feat(task): Define core data structures and API

---
+++ include/task_management.h
@@ -0,0 +1,47 @@
+#ifndef TASK_MANAGEMENT_H
+#define TASK_MANAGEMENT_H
+
+#include "config.h"
+#include <stdint.h>
+
+typedef struct {
+  uint32_t id;
+
+  uint32_t period[MAX_CRITICALITY_LEVELS];
+  uint32_t deadline[MAX_CRITICALITY_LEVELS];
+  uint32_t wcet[MAX_CRITICALITY_LEVELS];
+
+  uint8_t criticality_level;
+} Task;
+
+typedef enum {
+  JOB_STATE_IDLE,
+  JOB_STATE_READY,
+  JOB_STATE_RUNNING,
+  JOB_STATE_COMPLETED
+} JobState;
+
+typedef struct {
+  const Task *parent_task;
+  uint32_t arrival_time;
+  uint32_t absolute_deadline;
+  uint32_t virtual_deadline;
+  uint32_t remaining_wcet;
+
+  JobState state;
+
+  struct Job *next;
+} Job;
+
+void task_management_init();
+
+Task *create_task(uint32_t id, uint32_t period[], uint32_t deadline[],
+                  uint32_t wcet[], uint8_t criticality_level);
+
+Job *create_job(const Task *parent_task);
+void release_job(Job *job);
+void add_to_queue_sorted(Job **queue_head, Job *job);
+Job *peek_next_job(Job *queue_head);
+Job *pop_next_job(Job *queue_head);
+
+#endif
