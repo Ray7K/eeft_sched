d1fed898e17b192382e7f6db3769e66fcf026d90
Ray7K <173786719+Ray7K@users.noreply.github.com>
2025-10-27 17:54:37 +0530

    feat(ring buffer): Add clear function and optimize of concurrency

--- include/ring_buffer.h
+++ include/ring_buffer.h
@@ -6,8 +6,8 @@
 #include <string.h>
 
 typedef struct {
-  _Atomic uint64_t head;
-  _Atomic uint64_t tail;
+  _Atomic uint64_t head __attribute__((aligned(64)));
+  _Atomic uint64_t tail __attribute__((aligned(64)));
   _Atomic uint64_t *seq;
   uint64_t buf_size;
   uint64_t buf_elem_size;
@@ -98,12 +98,23 @@ static inline int ring_buffer_dequeue(ring_buffer_t *rb, void *elem) {
   return 0;
 }
 
-#define ring_buffer_iter_read_unsafe(rb, elem_ptr)                             \
-  for (uint64_t idx = atomic_load(&rb->head),                                  \
-                elem_ptr = (char *)rb->buffer +                                \
-                           (idx % rb->buf_size * rb->buf_elem_size);           \
-       idx < atomic_load(&rb->tail);                                           \
-       idx++, elem_ptr = (char *)rb->buffer +                                  \
-                         (idx % rb->buf_size * rb->buf_elem_size))
+static inline void ring_buffer_clear(ring_buffer_t *rb) {
+  uint64_t head = atomic_load_explicit(&rb->head, memory_order_relaxed);
+  uint64_t tail = atomic_load_explicit(&rb->tail, memory_order_relaxed);
+
+  for (uint64_t i = head; i < tail; i++) {
+    atomic_store_explicit(&rb->seq[i % rb->buf_size], i + rb->buf_size,
+                          memory_order_release);
+  }
+
+  atomic_store_explicit(&rb->head, tail, memory_order_release);
+}
+
+#define ring_buffer_iter_read_unsafe(rb, elem_ptr, elem_type)                  \
+  for (uint64_t idx = atomic_load(&(rb)->head);                                \
+       idx < atomic_load(&(rb)->tail); idx++)                                  \
+    if ((elem_ptr = (elem_type *)((char *)(rb)->buffer +                       \
+                                  (idx % (rb)->buf_size *                      \
+                                   (rb)->buf_elem_size))) != NULL)
 
 #endif
